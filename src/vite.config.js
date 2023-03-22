8dfde7af700ce10f50920dd87b01da4882502e63
Ryan Wong <ryanwong@ximnet.com.my>
2023-03-22 09:14:41 +0800

    add deploy workflow

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,17 +1,16 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable import/no-unresolved */
 import react from "@vitejs/plugin-react";
-import reactRefresh from "@vitejs/plugin-react-refresh";
 // import path from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), reactRefresh()],
+  plugins: [react()],
   base: "",
   root: "",
   build: {
     // outDir: path.join(__dirname, "test"),
-    outDir: "../test",
+    // outDir: "../test",
   },
 });
